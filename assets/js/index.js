5f7ae053feca8fea0cf06dffc49b50ad930662d1
justinschoi93 <justinschoi93@gmail.com>
2023-07-24 12:47:13 -0700

    output.html, prompts updated, working on .then

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,21 +2,36 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 inquirer
-    .prompt({
-        question: 'What is your name?', answer
+    .prompt([
+        {
+            question: 'What is your name?', 
+            name: 'usersName'
+        },
+        {
+            question: 'Where are you located?',
+            name: 'location'
+        },
+        {
+            question: 'Tell me about yourself...',
+            name: 'bio'
+        },
+        {
+            question: 'What is your linkedin URL?',
+            name: 'linkedInURL'
+        },
+        {
+            question: 'What is your gitHub URL?',
+            name: 'gitHubURL'
+        }
+    ])
+    .then(function(response){
+        const htmlcode = '';
+
+
+        fs.writeFile('output.html', JSON.stringify(response), (err) =>
+        err ? console.error(err) : console.log('Success!'))
     })
-    .prompt({
-        question: 'Where are you located?', answer
-    })
-    .prompt({
-        question: 'Tell me about yourself...', answer
-    })
-    .prompt({
-        question: 'What is your linkedin URL?', answer
-    })
-    .prompt({
-        question: 'What is your GitHub URL?', answer
-    })
+    
     //use a series of prompts
         //name
         //location
